7a4c51d165e7df0a3e07bc35547cb6aa22207ca3
Viplav jha <viplavjha1993@gmail.com>
2023-11-14 12:38:06 +0530

    Setup-NEXTUI

---
+++ app/(Providers)/NextUiProvider.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import {NextUIProvider} from "@nextui-org/react";
+
+interface ProviderProps {
+    children:React.ReactNode;
+}
+
+export default function Provider({children}:ProviderProps) {
+  // 2. Wrap NextUIProvider at the root of your app
+  return  <NextUIProvider>{children}</NextUIProvider> ;
+  
+}
