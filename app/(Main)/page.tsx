be12878ef8671ae4cfc1c00ccff2f7718fc217a4
Viplav jha <viplavjha1993@gmail.com>
2023-11-19 12:21:53 +0530

    page

--- app/(Main)/page.tsx
+++ app/(Main)/page.tsx
@@ -1,67 +1,90 @@
-'use client'
-import React,{useState,useEffect} from 'react'
-import Header from './components/Header'
-import Hero from './components/Route/Hero';
-import Image from 'next/image';
-import About from './components/Route/About';
-import { styles } from '../utils/style';
-import PromptCard from './components/Prompts/PromptCard';
-import BestSellers from './components/shop/BestSellers';
+"use client";
+import React, { useState, useEffect } from "react";
+import Header from "./components/Layout/Header";
+import Hero from "./components/Route/Hero";
+import Image from "next/image";
+import About from "./components/Route/About";
+import { styles } from "../utils/style";
+import PromptCard from "./components/Prompts/PromptCard";
+import BestSellers from "./components/shop/BestSellers";
+import Future from "./components/Route/Future";
+import Partners from "./components/Route/Partners";
+import SellersBanner from "./components/shop/SellersBanner";
+import { Divider } from "@nextui-org/react";
+import Footer from "./components/Layout/Footer";
+import axios from 'axios';
 
 type Props = {
-  activeItem:number;
-}
+  activeItem: number;
+};
 
 const Page = (props: Props) => {
- const [isMounted,setIsMounted] =useState(false);
+  const [isMounted, setIsMounted] = useState(false);
+  const [user,setUser] =useState();
 
-
-  useEffect(()=>{
-    if(!isMounted) {
-      setIsMounted(true)
+  useEffect(() => {
+    if (!isMounted) {
+      setIsMounted(true);
     }
-  },[isMounted])
+  }, [isMounted]);
 
-  if(!isMounted) {
+  useEffect(() => {
+    axios.get('/api/me')
+      .then((res) => {
+        console.log(res.data);
+        setUser(res.data.user); // Update the state with the user data
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  if (!isMounted) {
     return null;
   }
+ 
 
   return (
     <div>
-       <div className='banner'>
-       <Header activeItem={0}/>
-        <Hero/>
-       </div>
-        <Image
-          src={"https://pixner.net/aikeu/assets/images/footer/shape-two.png"}
-          width={120}
-          height={120}
-          alt=""
-          className='absolute right-[-30px]'
-        />
-        <br />
-        <div className='w-[95%] md:w-[90%] xl:w-[80%] 2xl:w-[75%] m-auto'>
-           <About/>
+      <div className="banner">
+        <Header activeItem={0} />
+        <Hero />
+      </div>
+      <Image
+        src={"https://pixner.net/aikeu/assets/images/footer/shape-two.png"}
+        width={120}
+        height={120}
+        alt=""
+        className="absolute right-[-30px]"
+      />
+      <br />
+      <div className="w-[95%] md:w-[90%] xl:w-[80%] 2xl:w-[75%] m-auto">
+        <About />
         <div>
           <h1 className={`${styles.heading} p-2 font-Monserrat`}>
-              Latest Prompts
+            Latest Prompts
           </h1>
-          <div className='flex flex-wrap'>
-            <PromptCard/>
-            <PromptCard/>
-            <PromptCard/>
-            <PromptCard/>
-            <PromptCard/>
-            <PromptCard/>
-           
+          <div className="flex flex-wrap">
+            <PromptCard />
+            <PromptCard />
+            <PromptCard />
+            <PromptCard />
+            <PromptCard />
+            <PromptCard />
           </div>
-          <br/>
-          <BestSellers/>
+          <br />
+          <BestSellers />
+          <Future />
+          <Partners />
+          <SellersBanner />
+          <br />
+          <br />
+          <Divider className="bg-[#ffffff23]" />
+          <Footer />
         </div>
+      </div>
     </div>
-    </div>
-   
-  )
-}
+  );
+};
 
 export default Page;
